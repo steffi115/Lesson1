6ab77e99176039c4a898021b0eb998d87e0764ee
steffi115 <chirpychurro@gmail.com>
2024-09-10 23:10:55 -0400

    first commit

---
+++ PythonNamingConvention.py
@@ -0,0 +1,9 @@
+#Question 2 -Task 1
+#: Code Correction You have been given a piece of code with several variable and 
+# constant names that don't follow the Python naming convention. 
+# Your task is to correct them:
+
+PI_VALUE = 3.14
+UserAge = 25
+user_location = "New York"
+MAXLIMIT = 1000 
